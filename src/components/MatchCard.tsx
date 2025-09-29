3b52048e8ecda7a553a2a625af4f202abd3d1bbe
arsya2405 <arsyanurafi@gmail.com>
2025-09-29 17:45:40 +0700

    Update MatchCard.tsx

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -7,7 +7,7 @@ interface MatchCardProps {
 
 export function MatchCard({ home, away, time, date }: MatchCardProps) {
   return (
-    <div className="w-56 h-[140px] bg-white rounded-xl shadow p-4 flex flex-col items-center justify-center">
+    <div className="w-56 h-[140px] bg-white rounded-xl shadow p-4 flex flex-col items-center justify-center flex-shrink-0">
       <div className="flex items-center justify-between w-full">
         <img src={home} className="w-10 h-10" />
         <span className="text-sm font-medium">VS</span>
