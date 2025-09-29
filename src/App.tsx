5db4f5c47d682132bcbe653be1401b4a29f35ad1
arsya2405 <arsyanurafi@gmail.com>
2025-09-29 22:10:26 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './page/Home.tsx';
-import About from './page/About.tsx';
 import Table from './page/Table.tsx';
 import Layout from './components/Layout.tsx';
 import Match from './page/Match.tsx';
@@ -12,9 +11,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
         <Route path="match" element={<Match />} />
         <Route path="table" element={<Table />} />
+        <Route path="news" element={<News />} />
       </Route>
 
         <Route path="*" element={<div>404 Not Found</div>} />
