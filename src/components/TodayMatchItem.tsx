efff2226c6e3d2f1ecb94b8b37862c08722a8f9f
arsya2405 <arsyanurafi@gmail.com>
2025-10-26 20:13:21 +0700

    Update TodayMatchItem.tsx

--- src/components/TodayMatchItem.tsx
+++ src/components/TodayMatchItem.tsx
@@ -1,35 +1,33 @@
+import type { Team } from "../types/Match";
+
+// butuh logo dan score dari team 1 dan 2
 interface TodayMatchItemProps {
-  homeLogo: string;
-  homeName: string;
-  awayLogo: string;
-  awayName: string;
-  score: string; // contoh: "2 - 1"
+  homeTeam: Team;
+  awayTeam: Team;
+  score_a: number;
+  score_b: number;
 }
 
-export default function TodayMatchItem({
-  homeLogo,
-  homeName,
-  awayLogo,
-  awayName,
-  score,
-}: TodayMatchItemProps) {
+export default function TodayMatchItem({ homeTeam, awayTeam, score_a, score_b }: TodayMatchItemProps) {
   return (
     <div className="flex items-center justify-between p-3">
       {/* Tim Home */}
       <div className="flex items-center gap-2 w-1/3">
-        <span className="text-sm font-medium">{homeName}</span>
-        <img src={homeLogo} alt={homeName} className="w-7" />
+        <span className="text-sm font-medium">{homeTeam.name}</span>
+        <img src={homeTeam.logo} alt={homeTeam.name} className="w-7" />
       </div>
 
       {/* Skor */}
-      <div className="text-sm font-bold">
-        -
+      <div className="flex text-sm font-bold">
+        {score_a}
+        <p> - </p>
+        {score_b}
       </div>
 
       {/* Tim Away */}
       <div className="flex items-center gap-2 w-1/3 justify-end">
-        <img src={awayLogo} alt={awayName} className="w-7" />
-        <span className="text-sm font-medium">{awayName}</span>
+        <img src={awayTeam.logo} alt={awayTeam.name} className="w-7" />
+        <span className="text-sm font-medium">{awayTeam.name}</span>
       </div>
     </div>
   );
