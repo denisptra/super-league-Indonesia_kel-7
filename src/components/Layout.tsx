8ef37e786fd55d4d6eb88dd2d18874302e55d617
arsya2405 <arsyanurafi@gmail.com>
2025-10-28 10:59:10 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,9 @@ import FooterComponent from './Footer';
 
 function Layout() {
   return (
-    <div className="layout-container">
+    <div className="layout-container flex flex-col min-h-screen">
       <NavbarComponent />
-      <main className="pt-18">
+      <main className="pt-18 flex-grow">
         <Outlet /> {/* <- nested routes akan dirender di sini */}
       </main>
       <FooterComponent />
